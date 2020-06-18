54ff54dbc1e3a238af8a84c199e0b9380de75648
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-06-18 16:22:51 -0500

    Got the CSS to link with the notes html. Got text to print in db file.

--- server.js
+++ server.js
@@ -11,9 +11,66 @@ const PORT = process.env.PORT || 3000;
 // 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// routes
+
+// main page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+//notes page
+ app.get("*", function(req, res) {
+   res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+}); 
+
+
+//db
+ app.get("/", function(req, res) {
+   res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
+ });
+ 
+ app.use(express.urlencoded({ extended: true}));
+ app.use(express.json());
+ app.use(express.static("./Develop/public"));
+
+ //printing in db
+ 
+ app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "/Develop/db/db.json"), "utf8", function (error, data) {
+      if (error) {
+        console.log(error);
+      }
+ 
+      const noteInfo = JSON.parse(data);
+ 
+      const newEntry = {
+        id: noteInfo.length + 1,
+        title: req.body.title,
+        text:req.body.text
+      }
+ 
+ 
+    noteInfo.push(newEntry);
+    res.json(newEntry);
+ 
+    fs.writeFile(path.join(__dirname, "/Develop/db/db.json"), JSON.stringify(noteInfo, null, 2), function (err) {
+      if (err) throw err;
+    });
+ 
+   });
+ });
+
+ //delete
+
+ //app.delete('/api/notes', function (req, res) {
+  //})
+  
+
+
+
 
 // listening to port
 app.listen(PORT, function() {
